84c8e1a1c1bcfc77f70afdb49dceee25a9f19681
FUNFIA-SMIE <informaticienfunfiasmie@gmail.com>
2025-07-21 09:44:34 +0300

    5f

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connexion à MongoDB
-mongoose.connect('mongodb://localhost:27017/synchronisation')
-  .then(() => console.log('MongoDB connecté'))
-  .catch(err => console.error('Erreur MongoDB :', err));
-
-const db = mongoose.connection;
-
-// Import des routes
 const evenementsRoutes = require('./routes/evenements');
-app.use('/evenements', evenementsRoutes(db));
+app.use('/evenements', evenementsRoutes());
 
 const PORT = 3000;
 app.listen(PORT, () => {
