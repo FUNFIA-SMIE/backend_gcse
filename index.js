1059647334c3fd682f039940b1922fbedf098597
FUNFIA-SMIE <informaticienfunfiasmie@gmail.com>
2025-11-17 13:56:50 +0300

    ars

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const delete_ = require('./routes/delete');
 
 const path = require('path');
 const inspection = require('./routes/inspection');
-
+const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({
